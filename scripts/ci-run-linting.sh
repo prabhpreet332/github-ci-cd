e2ba28577f7714f5c5e9220e31d720842cb2e913
Prabhpreet Singh <51478897+prabhpreet332@users.noreply.github.com>
2023-01-27 01:16:02 +0530

    Update ci-run-linting.sh

--- scripts/ci-run-linting.sh
+++ scripts/ci-run-linting.sh
@@ -1,6 +1,5 @@
 CHECK_FLAG="--check"
 BASE_DIR=./
-echo $BASE_DIR
 
 python3 -m black $CHECK_FLAG --exclude migrations --line-length=88 $BASE_DIR
 python3 -m isort $CHECK_FLAG --profile black --skip migrations $BASE_DIR
