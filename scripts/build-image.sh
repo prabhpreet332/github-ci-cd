7bf6f9dbfc49e5514936d90e6c42c7261936be7e
Prabhpreet Singh <psinghoff332@gmail.com>
2023-02-10 17:59:35 +0530

    update: script to build image

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,6 +1,22 @@
 # $1 - username
 # $2 - imagename
 # $3 - tag - branch name
-docker build -t $2 .
-docker tag $2 $1/$2:$3
-docker push $1/$2:$3
+# IMAGE_TAG=echo "$3" | sed -r 's/[//]+/_/g'
+
+# bash ./scripts/build-image.sh "ps332" "prabhpreet332/github-ci-cd" "refs/heads/feature/tag-creation" 
+
+# BRANCH_NAME=$3
+# IMAGE_TAG=${$3 | sed -r 's/[//]+/_/g'}
+# IMAGE_TAG="$3" | tr / _
+
+REPOSITORY_NAME=$2
+ARR_REPOSITORY_NAME=(${REPOSITORY_NAME///// })
+# echo $ARR_REPOSITORY_NAME
+IMAGE_NAME=${ARR_REPOSITORY_NAME[1]}
+
+IMAGE_TAG=`echo "$3" | sed -r 's/[//]+/_/g'`
+echo "Building image for image:tag -> " $IMAGE_NAME $IMAGE_TAG  
+
+docker build -t $IMAGE_NAME .
+docker tag $IMAGE_NAME $1/$IMAGE_NAME:$IMAGE_TAG
+docker push $1/$IMAGE_NAME:$IMAGE_TAG
